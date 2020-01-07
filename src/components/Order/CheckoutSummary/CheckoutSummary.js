07ab71b22c9ac5855325cfc82483d44a834e63e4
BhaskarShelar <Bhaskar.Shelar99@gmail.com>
2020-01-07 09:28:55 +0530

    router package

---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Burger from '../../Burger/Burger';
+import classes from './CheckoutSummary.module.css';
+import Button from '../../UI/Button/Button';
+const checkoutSummary = (props) => {
+
+    return (
+        <div className={classes.CheckoutSummary}>
+            <h1>we hope it teste well!</h1>
+            <div style={{ width: "100%",  margin: "auto" }}>
+                <Burger ingredients={props.ingredtents} />
+            </div>
+            <Button
+                btnType="Danger">CANCEL</Button>
+            <Button
+                btnType="Success">CONTINUE</Button>
+        </div>
+    )
+}
+export default checkoutSummary;
---
+++ src/containers/Checkout/Checkout.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+
+class Checkout extends Component {
+    state={
+        ingredtents:{
+            salad:1,
+            bacon:1,
+            meat:1,
+            cheese:1
+        }
+    }
+    render(){
+        return(
+            <div>
+                <CheckoutSummary ingredtents={this.state.ingredtents} />
+            </div>
+        )
+    }
+}
+
+export default Checkout;
